b03df6d85989af2acacc212cf3c5bd515d8fad2b
vereczkibalint <vereczkibalint@gmail.com>
2020-11-02 09:12:52 +0100

    added router to app and connecting to database

--- server/server.js
+++ server/server.js
@@ -3,13 +3,20 @@ const helmet = require('helmet');
 
 const app = express();
 
+const connectToMongoDB = require('./config/db');
+const lecturerRouter = require('./routes/lecturer.routes');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 
+connectToMongoDB();
+
 app.get('/', (req, res) => {
     res.send('It works!');
 });
+
+app.use('/api/lecturers', lecturerRouter);
 
 const PORT = process.env.PORT || 3001;
 
