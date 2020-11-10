65da97fa7c16f1dc5f29097efdb3a517afd81b93
vereczkibalint <vereczkibalint@gmail.com>
2020-11-10 22:49:22 +0100

    added validation

--- server/models/Consultation.js
+++ server/models/Consultation.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
+const commonValidator = require('./validators/common.validators');
 const Schema = mongoose.Schema;
 
 const ConsultationSchema = new Schema({
     lecturer: {
         type: Schema.Types.ObjectId,
         ref: 'lecturer',
-        required: [true, 'Oktató megadása kötelező!']
+        required: [true, 'Oktató megadása kötelező!'],
+        validate: [
+            { validator: commonValidator.isValidObjectId, message: 'Hibás oktató azonosító!' }
+        ]
     },
     startTime: {
         type: Date,
@@ -14,7 +18,12 @@ const ConsultationSchema = new Schema({
     },
     endTime: {
         type: Date,
-        required: [true, 'Befejezés időpontjának megadása kötelező!']
+        required: [true, 'Befejezés időpontjának megadása kötelező!'],
+        validate: [
+            { validator: function(endTimeValue) {
+                return this.startTime < endTimeValue;
+            }, message: 'A befejezési időpontnak későbbinek kell lennie a kezdési időponttól!' }
+        ]
     },
     isAvailable: {
         type: Boolean,
