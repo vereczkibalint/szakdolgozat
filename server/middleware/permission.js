b61837fca0e607df8afac0f84246e6a88508fecf
vereczkibalint <vereczkibalint@gmail.com>
2020-11-19 14:19:53 +0100

    merged 3 methods to one

--- server/middleware/permission.js
+++ server/middleware/permission.js
@@ -1,49 +1,19 @@
 const { ApiError, handleApiError } = require('../services/errors/ApiError');
 
-exports.isAdmin = (req, res, next) => {
-  const user = req.user;
+exports.checkPermission = (permissions) => {
+  return (req, res, next) => {
+    const user = req.user;
 
-  if (!user) {
-    let err = new ApiError(401, 'Autentikációs hiba!');
-    return handleApiError(err, res);
-  }
-
-  if(user.role !== 'ADMIN') {
-    let err = new ApiError(403, 'Hozzáférés megtagadva!');
-    return handleApiError(err, res);
-  } else {
-      next();
-  }
-};
-
-exports.isLecturer = (req, res, next) => {
-  const user = req.user;
+    if(!user) {
+      let err = new ApiError(401, 'Autentikációs hiba!');
+      return handleApiError(err, res);
+    }
   
-  if (!user) {
-    let err = new ApiError(401, 'Autentikációs hiba!');
-    return handleApiError(err, res);
-  }
-
-  if(user.role !== 'LECTURER') {
-    let err = new ApiError(403, 'Hozzáférés megtagadva!');
-    return handleApiError(err, res);
-  } else {
-      next();
-  }
-};
-
-exports.isStudent = (req, res, next) => {
-  const user = req.user;
+    if(!permissions.includes(user.role)) {
+      let err = new ApiError(403, 'Hozzáférés megtagadva!');
+      return handleApiError(err, res);
+    }
   
-  if (!user) {
-    let err = new ApiError(401, 'Autentikációs hiba!');
-    return handleApiError(err, res);
+    next();
   }
-
-  if(user.role !== 'STUDENT') {
-    let err = new ApiError(403, 'Hozzáférés megtagadva!');
-    return handleApiError(err, res);
-  } else {
-      next();
-  }
-};
+}
