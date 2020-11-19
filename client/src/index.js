bf8ed87f29f708c9749eef43860f16fa9bc4281a
vereczkibalint <vereczkibalint@gmail.com>
2020-11-19 21:09:56 +0100

    imported necessary styles and js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
