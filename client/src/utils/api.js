ebd02c3f7df709b97da15af8af47a36258a53a9c
vereczkibalint <vereczkibalint@gmail.com>
2020-12-06 22:10:41 +0100

    added backend url to axios config

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://localhost:3001/api',
     headers: {
         'Content-Type': 'application/json'
     }
