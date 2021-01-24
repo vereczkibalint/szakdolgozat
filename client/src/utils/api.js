5ad79058879918b7f9b8ccad1a2a13614ef068d8
vereczkibalint <vereczkibalint@gmail.com>
2021-01-24 21:25:29 +0100

    added heroku link for deployment testing purposes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
+    //baseURL: 'https://szakdoga-kovetorendszer.herokuapp.com/api',
     baseURL: 'http://localhost:3001/api',
     headers: {
         'Content-Type': 'application/json'
