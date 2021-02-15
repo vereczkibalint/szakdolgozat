8ca6d7395fc87304492e40ee741c64901eb3cb95
vereczkibalint <vereczkibalint@gmail.com>
2021-02-15 10:16:28 +0100

    password change message is now showing if password change was successful
    minor styling changes, bootstrap password inputs has a margin-bottom of 20px but text did not have any

--- client/src/app/components/Settings/ChangePasswordForm.js
+++ client/src/app/components/Settings/ChangePasswordForm.js
@@ -8,11 +8,12 @@ const ChangePasswordForm = () => {
     const dispatch = useDispatch();
 
     const errorMessage = useSelector(state => state.auth.error.message);
+    const passwordChangeMessage = useSelector(state => state.auth.passwordChangeMessage);
 
     const [showPassword, setShowPassword] = useState(false);
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    const [newPasswordConfirm, setnewPasswordConfirm] = useState('');
+    const [newPasswordConfirm, setNewPasswordConfirm] = useState('');
 
     const canSubmit = currentPassword.length >= 8 && newPassword.length >= 8 && newPassword === newPasswordConfirm;
 
@@ -29,6 +30,8 @@ const ChangePasswordForm = () => {
     return (
         <div className="mx-auto w-75 mt-3">
             { errorMessage && <Alert type="danger" message={errorMessage}/> }
+            { passwordChangeMessage && <Alert type="success" message={passwordChangeMessage} /> }
+
             <Form className="p-3" style={{border: '.2rem solid #ececec', borderRadius: '10px'}}>
                 <Form.Check label="Jelszavak mutatása" className="text-right mb-1" onChange={(e) => setShowPassword(e.target.checked)} />
                 <Form.Group>
@@ -36,6 +39,7 @@ const ChangePasswordForm = () => {
                     <Form.Control
                         id="current_password"
                         type={showPassword ? "text" : "password"}
+                        style={{ marginBottom: '20px' }}
                         value={currentPassword}
                         onChange={(e) => setCurrentPassword(e.target.value)}
                     />
@@ -46,6 +50,7 @@ const ChangePasswordForm = () => {
                         id="new_password"
                         type={showPassword ? "text" : "password"}
                         value={newPassword}
+                        style={{ marginBottom: '20px' }}
                         isInvalid={newPasswordInvalid}
                         onChange={(e) => setNewPassword(e.target.value)}
                     />
@@ -59,8 +64,9 @@ const ChangePasswordForm = () => {
                         id="new_password_confirm"
                         type={showPassword ? "text" : "password"}
                         value={newPasswordConfirm}
+                        style={{ marginBottom: '20px' }}
                         isInvalid={passwordConfirmInvalid}
-                        onChange={(e) => setnewPasswordConfirm(e.target.value)}
+                        onChange={(e) => setNewPasswordConfirm(e.target.value)}
                     />
                     { passwordConfirmInvalid && (
                         <Form.Control.Feedback type="invalid">
