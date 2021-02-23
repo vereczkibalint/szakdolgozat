8d245b5f85ccc234674d8bb7bac7191b2cb91694
vereczkibalint <vereczkibalint@gmail.com>
2021-02-23 12:27:08 +0100

    added import button to lecturers and students (admin subapp)

--- client/src/admin/pages/lecturers/ManageLecturers.js
+++ client/src/admin/pages/lecturers/ManageLecturers.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
@@ -6,7 +6,10 @@ import Button from 'react-bootstrap/Button';
 import LoadingSpinner from '../../../common/components/Loading/LoadingSpinner';
 
 import Datatable from '../../components/Datatable/Datatable';
-import { fetchAllLecturer } from '../../services/userService';
+import {fetchAllLecturer, importUsers} from '../../services/userService';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import Alert from "../../../common/components/Alert";
 
 const ManageLecturers = () => {
     const history = useHistory();
@@ -17,7 +20,11 @@ const ManageLecturers = () => {
     }, [dispatch]);
 
     let lecturers = useSelector(state => state.users.lecturers);
+    let errorMessage = useSelector(state => state.users.errors.message);
+    let importErrors = useSelector(state => state.users.errors.errors);
     let isLoading = useSelector(state => state.users.isLoading);
+
+    let importInputRef = useRef(null);
 
     function redirectToCreateLecturerPage() {
         history.push('/admin/dashboard/lecturer/create');
@@ -36,18 +43,38 @@ const ManageLecturers = () => {
         );
     }
 
+    function handleFileImport(importFile) {
+        dispatch(importUsers(importFile, 'lecturer'));
+    }
+
     return (
         <div className="mt-3">
             <h2 className="m-3 text-center">
                 Oktatók kezelése
             </h2>
 
-            <Button variant="primary" className={"mb-2"} onClick={redirectToCreateLecturerPage}>
-                Új felvétele
-            </Button>
+            <div className="d-flex justify-content-between">
+                <Button variant="primary" className={"mb-2"} onClick={redirectToCreateLecturerPage}>
+                    Új felvétele
+                </Button>
+
+                <input type="file" ref={importInputRef} style={{ display: 'none' }} onChange={(e) => handleFileImport(e.target.files[0])} />
+
+                <Button variant="success" className="mr-3 mb-2" onClick={() => importInputRef.current.click()}>
+                    <FontAwesomeIcon icon={faPlus} /> Importálás
+                </Button>
+            </div>
+
+            { errorMessage && <Alert type="danger" message={errorMessage} /> }
+
+            { importErrors && importErrors.map((importError, i) => {
+                return (
+                    <Alert type="danger" message={importError.message} key={i} />
+                );
+            }) }
 
             { !isLoading &&
-            <Datatable headers={headers} body={lecturers} history={history}/>
+                <Datatable headers={headers} body={lecturers} history={history}/>
             }
         </div>
     );
--- client/src/admin/pages/students/ManageStudents.js
+++ client/src/admin/pages/students/ManageStudents.js
@@ -1,19 +1,26 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 import LoadingSpinner from '../../../common/components/Loading/LoadingSpinner';
 import Datatable from '../../components/Datatable/Datatable';
 import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { fetchAllStudent } from '../../services/userService';
+import {fetchAllStudent, importUsers} from '../../services/userService';
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import Alert from "../../../common/components/Alert";
 
 const ManageStudents = () => {
     const history = useHistory();
     const dispatch = useDispatch();
 
     let students = useSelector(state => state.users.students);
+    let errorMessage = useSelector(state => state.users.errors.message);
+    let importErrors = useSelector(state => state.users.errors.errors);
     let isLoading = useSelector(state => state.users.isLoading);
+
+    let importInputRef = useRef(null);
 
     useEffect(() => {
         dispatch(fetchAllStudent());
@@ -36,17 +43,36 @@ const ManageStudents = () => {
         );
     }
 
+    function handleFileImport(importFile) {
+        dispatch(importUsers(importFile, 'student'));
+    }
+
     return (
         <div className="mt-3">
             <h2 className="m-3 text-center">
                 Hallgatók kezelése
             </h2>
 
-            <div className="d-flex justify-content-start">
+            <div className="d-flex justify-content-between">
                 <Button variant="primary" className="mr-3 mb-2" onClick={redirectToCreateStudentPage}>
                     Új felvétele
                 </Button>
+
+                <input type="file" ref={importInputRef} style={{ display: 'none' }} onChange={(e) => handleFileImport(e.target.files[0])} />
+
+                <Button variant="success" className="mr-3 mb-2" onClick={() => importInputRef.current.click()}>
+                    <FontAwesomeIcon icon={faPlus} /> Importálás
+                </Button>
+
             </div>
+
+            { errorMessage && <Alert type="danger" message={errorMessage} /> }
+
+            { importErrors && importErrors.map((importError, i) => {
+                return (
+                    <Alert type="danger" message={importError.message} key={i} />
+                );
+            }) }
 
             { !isLoading &&
                 <Datatable headers={headers} body={students} history={history}/>
